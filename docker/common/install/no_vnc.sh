d7fc780b8f96d716513df59af9a9faf564641201
Youcef Islam Remichi <youcef.islam.remichi@gmail.com>
2019-07-07 23:33:42 +0100

    noVNC: setup index.html

--- docker/common/install/no_vnc.sh
+++ docker/common/install/no_vnc.sh
@@ -9,5 +9,18 @@ wget -qO- https://github.com/novnc/noVNC/archive/v1.1.0.tar.gz | tar xz --strip 
 # use older version of websockify to prevent hanging connections on offline containers, see https://github.com/ConSol/docker-headless-vnc-container/issues/50
 wget -qO- https://github.com/novnc/websockify/archive/v0.8.0.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME/utils/websockify
 chmod +x -v $NO_VNC_HOME/utils/*.sh
-## create index.html to forward automatically to `vnc.html`
-ln -s $NO_VNC_HOME/vnc.html $NO_VNC_HOME/index.html
+## create index.html for noVNC
+#ln -s $NO_VNC_HOME/vnc.html $NO_VNC_HOME/index.html
+echo \
+"<!DOCTYPE html>\n" \
+"<html>\n" \
+"    <head>\n" \
+"        <title>noVNC</title>\n" \
+"        <meta charset=\"utf-8\"/>\n" \
+"    </head>\n" \
+"    <body>\n" \
+"        <p><a href=\"vnc_lite.html\">noVNC Lite Client</a></p>\n" \
+"        <p><a href=\"vnc.html\">noVNC Full Client</a></p>\n" \
+"    </body>\n" \
+"</html>" \
+> ${NO_VNC_HOME}/index.html
